c50a97f172aa43893f35e40e7973265d116093f9
tesstest <dzaymak@gmail.com>
2017-12-17 20:06:42 -0800

    lesson 4.

---
+++ src/com/company/Lesson4.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lesson4 {
+
+    public static void main(String[] args) {
+        findMin();
+        sortArray();
+        palindrom();
+    }
+
+    public static void findMin() {
+        //Task 3. Find min in array
+        int[] array = {8, 9, 6, 1, 7, 10, 100, 13, 0, 65, 89, 2, -90};
+        //assume that 0th element is min
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.println("Minimum is " + min);
+    }
+
+    public static void sortArray() {
+        //Task 5. Sort an array
+        int[] array = {8, 9, 6, 1, 7, 10, 100, 13, 0, 65, 89, 2, -90};
+        int temp;
+        boolean isSorted = false;
+        int endOfArray = array.length - 1;
+
+        //bubble sort
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < endOfArray; i++) {
+                if ((array[i] > array[i + 1])) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            endOfArray--;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void palindrom(){
+        //Task 6. Palindrom
+        String text;
+        boolean sameChars = true;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string:");
+
+        text = scan.nextLine();
+        text = text.toLowerCase();
+        text = text.replaceAll("\\s+","");
+
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++){
+            if (chars[i] != chars[chars.length - 1- i]){
+                sameChars = false;
+            }
+        }
+
+        if (sameChars){
+            System.out.println("It is palindrom!");
+        } else {
+            System.out.println("It is not palindrom!");
+        }
+    }
+}
